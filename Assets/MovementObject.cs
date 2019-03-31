e9a9e3199178913004b839b27d8a9fad004df6ae
Gabriel Jay Huerte <gjhuerte@gmail.com>
2019-03-31 18:18:12 +0800

    Initial commit

---
+++ Assets/MovementObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementObject : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 20000f;
+    public float sidewayForce = 500f;
+
+    public void MoveLeft()
+    {
+        rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    public void MoveRight()
+    {
+        rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardForce = 20000f;
+    public float sidewayForce = 500f;
+
+    // Use this for initialization
+    // void Start () {
+    //     rb.AddForce(0, 200, 500);
+    // }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime * 2, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+
+    }
+
+    public void MoveLeft()
+    {
+        rb.AddForce(-sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+
+    public void MoveRight()
+    {
+        rb.AddForce(sidewayForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+    }
+}
